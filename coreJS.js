5682895b8bfe62e55dd833ccda51c033ba4846f1
Vivian Zitek <vivian.zitek@gmail.com>
2023-02-21 00:23:55 -0700

    Begin work on basic input interpretation.

--- coreJS.js
+++ coreJS.js
@@ -10,3 +10,16 @@ function Delete() {
 	inputField = inputField.slice(0, -1);
 	document.getElementById('textto').innerHTML = inputField;
 }
+
+function ClearDisplay() {
+	inputField = "";
+	document.getElementById('textto').innerHTML = inputField;
+}
+
+function CalculateInput(rawInput) {
+
+	const charArray = [];
+	for (let char of rawInput) {
+		charArray.push(char);
+	}
+}
